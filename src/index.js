d3299186144d0647a9a4002c7dd2f96e81f5bea7
aonic7 <arsenal.rvp41@gmail.com>
2021-08-19 23:33:13 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+    if (typeof array != "undefined" && array != null && array.length != null && array.length > 0) {
+        let compnum = array[0];
+        for (let i = 0; i < array.length - 1; i++) {
+            if (array[i] < compnum) {
+                compnum = array[i]
+            }
+        }
+        return compnum;
+    } else {
+        return 0;
+    }
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (typeof array != "undefined" && array != null && array.length != null && array.length > 0) {
+        let compnum = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > compnum) {
+                compnum = array[i]
+            }
+        }
+        return compnum;
+    } else {
+        return 0;
+    }
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (typeof array != "undefined" && array != null && array.length != null && array.length > 0) {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        return sum / array.length;
+    } else {
+        return 0;
+    }
 }
